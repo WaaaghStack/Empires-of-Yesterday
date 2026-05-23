6ea5f4dd47999f5be830f280ee7736eb16466546
WaaaghStack <147443889+WaaaghStack@users.noreply.github.com>
2026-05-23 00:05:11 -0400

    Add Enemy class and basic combat system

---
+++ Enemy.gd
@@ -0,0 +1,8 @@
+# Enemy.gd
+class_name Enemy
+extends Resource
+
+@export var enemy_name: String = "Enemy"
+@export var health: int = 50
+@export var damage: int = 15
+@export var speed: float = 100.0
--- TacticalMap.gd
+++ TacticalMap.gd
@@ -2,7 +2,10 @@
 extends Control
 
 var selected_soldiers: Array[SoldierResource] = []
-var deployed_soldiers: Dictionary = {}
+var deployed_soldiers: Dictionary = {}  # slot_index -> soldier
+var enemies: Array = []
+
+var game_active := false
 
 @onready var map_grid: GridContainer = $MapGrid
 @onready var start_button: Button = $StartButton
@@ -35,8 +38,100 @@ func _on_slot_pressed(slot_index: int, slot_button: Button):
         slot_button.disabled = true
 
 func _on_start_pressed():
-    print("Mission started with ", deployed_soldiers.size(), " soldiers!")
-    # TODO: Implement enemy spawning and combat
+    if deployed_soldiers.size() == 0:
+        print("Deploy at least one soldier first!")
+        return
+    
+    game_active = true
+    start_button.disabled = true
+    print("Mission started!")
+    spawn_initial_enemies()
+    start_combat_loop()
 
-func _on_back_pressed():
-    get_tree().change_scene_to_file("res://SquadSelection.tscn")
+func spawn_initial_enemies():
+    print("Spawning enemies...")
+    for i in range(3):
+        var enemy = {
+            "name": "Enemy " + str(i + 1),
+            "health": 50,
+            "damage": 15,
+            "alive": true
+        }
+        enemies.append(enemy)
+        print("Spawned: ", enemy.name)
+    
+    # Start enemy attack timer
+    var timer = Timer.new()
+    timer.wait_time = 2.0
+    timer.one_shot = false
+    timer.timeout.connect(_enemy_attack_phase)
+    add_child(timer)
+    timer.start()
+
+func start_combat_loop():
+    print("Combat loop started!")
+    # Soldiers attack first
+    _soldier_attack_phase()
+
+func _soldier_attack_phase():
+    if not game_active:
+        return
+    
+    print("--- Soldier Attack Phase ---")
+    var alive_enemies = enemies.filter(func(e): return e.alive)
+    
+    if alive_enemies.size() == 0:
+        _victory()
+        return
+    
+    for slot_index in deployed_soldiers:
+        var soldier = deployed_soldiers[slot_index]
+        if alive_enemies.size() > 0:
+            var target = alive_enemies[0]
+            target.health -= soldier.damage
+            print(soldier.soldier_name, " attacks ", target.name, " for ", soldier.damage, " damage!")
+            
+            if target.health <= 0:
+                target.alive = false
+                print(target.name, " defeated!")
+    
+    # Check if all enemies dead
+    alive_enemies = enemies.filter(func(e): return e.alive)
+    if alive_enemies.size() == 0:
+        _victory()
+
+func _enemy_attack_phase():
+    if not game_active:
+        return
+    
+    print("--- Enemy Attack Phase ---")
+    var alive_soldiers = []
+    for slot_index in deployed_soldiers:
+        alive_soldiers.append(deployed_soldiers[slot_index])
+    
+    if alive_soldiers.size() == 0:
+        _defeat()
+        return
+    
+    var alive_enemies = enemies.filter(func(e): return e.alive)
+    for enemy in alive_enemies:
+        if alive_soldiers.size() > 0:
+            var target = alive_soldiers[0]
+            # Find which slot this soldier is in
+            for slot_index in deployed_soldiers:
+                if deployed_soldiers[slot_index] == target:
+                    # Soldier takes damage
+                    print(enemy.name, " attacks ", target.soldier_name, " for ", enemy.damage, " damage!")
+                    # Note: In a full implementation, we'd track soldier health
+                    break
+    
+    # Continue combat
+    _soldier_attack_phase()
+
+func _victory():
+    game_active = false
+    print("VICTORY! All enemies defeated!")
+
+func _defeat():
+    game_active = false
+    print("DEFEAT! All soldiers lost!")
