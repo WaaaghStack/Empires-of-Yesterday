bc560880d374bff347ea00e8828e93be428459f1
WaaaghStack <147443889+WaaaghStack@users.noreply.github.com>
2026-05-22 23:53:38 -0400

    Fix SoldierCard.gd node paths and setup logic

--- SoldierCard.gd
+++ SoldierCard.gd
@@ -8,20 +8,28 @@ signal deselected(soldier: SoldierResource)
 var soldier_data: SoldierResource
 var is_selected := false
 
-@onready var portrait = $MarginContainer/VBoxContainer/Portrait
-@onready var name_label = $MarginContainer/VBoxContainer/Name
-@onready var stats_label = $MarginContainer/VBoxContainer/Stats
-@onready var checkbox = $MarginContainer/VBoxContainer/CheckBox
+var portrait: TextureRect
+var name_label: Label
+var stats_label: Label
+var checkbox: CheckBox
+
+func _ready():
+    portrait = $MarginContainer/VBoxContainer/Portrait
+    name_label = $MarginContainer/VBoxContainer/Name
+    stats_label = $MarginContainer/VBoxContainer/Stats
+    checkbox = $MarginContainer/VBoxContainer/CheckBox
+    
+    if checkbox:
+        checkbox.toggled.connect(_on_checkbox_toggled)
 
 func setup(soldier: SoldierResource):
     soldier_data = soldier
-    if soldier.portrait:
+    if portrait and soldier.portrait:
         portrait.texture = soldier.portrait
-    name_label.text = soldier.soldier_name
-    stats_label.text = "HP: %d  DMG: %d" % [soldier.health, soldier.damage]
-
-func _ready():
-    checkbox.toggled.connect(_on_checkbox_toggled)
+    if name_label:
+        name_label.text = soldier.soldier_name
+    if stats_label:
+        stats_label.text = "HP: %d  DMG: %d" % [soldier.health, soldier.damage]
 
 func _on_checkbox_toggled(toggled_on: bool):
     is_selected = toggled_on
