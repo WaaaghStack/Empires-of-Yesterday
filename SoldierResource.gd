913ef3f7a03d2b5deee84a2b13efa2e103357c27
WaaaghStack <147443889+WaaaghStack@users.noreply.github.com>
2026-05-22 21:20:32 -0400

    Initial commit - SoldierResource

---
+++ SoldierResource.gd
@@ -0,0 +1,11 @@
+# SoldierResource.gd
+class_name SoldierResource
+extends Resource
+
+@export var soldier_name: String = "Rookie"
+@export var health: int = 100
+@export var damage: int = 25
+@export var speed: float = 120.0
+@export var portrait: Texture2D
+
+@export var description: String = "Standard Rifleman"
